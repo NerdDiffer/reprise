a3db6ad5dccdbecbf67c349ad329bed19ce051a1
Frances <franyang99@gmail.com>
2016-08-10 15:46:09 -0700

    Edit to follow style guide

--- client/src/peer.js
+++ client/src/peer.js
@@ -1,20 +1,21 @@
-var SimplePeer = require('simple-peer');
+const SimplePeer = require('simple-peer');
 
-var io = require('socket.io-client');
-var socket = io();
+const io = require('socket.io-client');
 
-var peerConnections = {};
+const socket = io();
+
+const peerConnections = {};
 
 const options = { config: { iceServers: [{ url: 'stun.l.google.com:19302' }] }, trickle: true };
 
 // someone has joined the room
-socket.on('new.peer', function(sockets) {
-  var length = sockets.length;
-  var initiatorId = sockets[length - 1];
+socket.on('new.peer', sockets => {
+  const length = sockets.length;
+  const initiatorId = sockets[length - 1];
   if (length > 1) {
     // this client will act as initiator for the new connections
     if (initiatorId === socket.id) {
-      //peerConnections = {};
+      // peerConnections = {};
       startConnection(sockets, 0);
     // client will act as remote waiting for initiator's offer
     } else {
@@ -23,9 +24,9 @@ socket.on('new.peer', function(sockets) {
   }
 });
 
-socket.on('answer', function(data) {
+socket.on('answer', data => {
   if (data.to === socket.id) {
-    var connection = peerConnections[data.by];
+    const connection = peerConnections[data.by];
     if (!connection.connected) {
       connection.peer.signal(data.answer);
       connection.connected = true;
@@ -33,25 +34,25 @@ socket.on('answer', function(data) {
   }
 });
 
-socket.on('full', function() {
+socket.on('full', () => {
   console.log('room is full');
 });
 
 // join a room
-var room = 'test';
+const room = 'test';
 socket.emit('join', room);
 
 // set up connection as initiator
 function startConnection(sockets, number) {
-  var peer = new SimplePeer(Object.assign({}, {initiator: true}));
-  peerConnections[sockets[number]] = { peer: peer, connected: false, by: socket.id, to: sockets[number] };
+  const peer = new SimplePeer(Object.assign(options, { initiator: true }));
+  peerConnections[sockets[number]] = { peer, connected: false, by: socket.id, to: sockets[number] };
 
-  peer.on('signal', function(data) {
+  peer.on('signal', data => {
     socket.emit('offer', { offer: data, by: socket.id, to: sockets[number] });
   });
 
-  peer.on('connect', function() {
-    peer.send('initiator ' + socket.id + ' saying hello');
+  peer.on('connect', () => {
+    peer.send(`initiator ${socket.id} saying hello`);
 
     // check if we need to make more connections
     if (number < sockets.length - 1) {
@@ -59,32 +60,32 @@ function startConnection(sockets, number) {
     }
   });
 
-  peer.on('data', function(data) {
-    console.log('initiator received message: ' + data);
+  peer.on('data', data => {
+    console.log(`initiator received message: ${data}`);
   });
 }
 
 // act as remote
 function receiveConnection(initiatorId) {
-  var peer = new SimplePeer(Object.assign({}, {initiator: false}));
-  peerConnections[initiatorId] = { peer: peer, connected: false, by: initiatorId, to: socket.id };
-  peer.on('signal', function(data) {
+  const peer = new SimplePeer(Object.assign(options, { initiator: false }));
+  peerConnections[initiatorId] = { peer, connected: false, by: initiatorId, to: socket.id };
+  peer.on('signal', data => {
     socket.emit('answer', { answer: data, by: socket.id, to: initiatorId });
   });
 
-  socket.on('offer', function(data) {
+  socket.on('offer', data => {
     if (data.to === socket.id) {
       peer.signal(data.offer);
     }
   });
 
-  peer.on('connect', function() {
+  peer.on('connect', () => {
     peerConnections[initiatorId].connected = true;
-    peer.send('i am ' + socket.id + ' saying hello');
+    peer.send(`i am ${socket.id} saying hello`);
   });
 
-  peer.on('data', function(data) {
-    console.log('received message: ' + data);
+  peer.on('data', data => {
+    console.log(`received message: ${data}`);
   });
 }
 
