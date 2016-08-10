b193dd3d203a952cd2366ca2a8a0885fee120a85
Rafael Espinoza <NerdDiffer@users.noreply.github.com>
2016-08-10 11:11:34 -0700

    File/dir structure, eslint & basic package json (#1)
    * Sketch out basic file structure
    
    * Add basic eslint config
    
    * Add package json
    
    * Add deps on eslint
    
    * Add many deps
    
    * Add webpack config
    
    * Update npm scripts
    
    * Update gitignore

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const DIST_DIR = path.join(__dirname, 'client', 'public');
+
+const config = {
+  devtool: 'inline-sourcemap',
+  resolve: {
+    modulesDirectories: ['node_modules', 'components'],
+    extensions: ['', '.js', '.jsx']
+  },
+  entry: path.join(SRC_DIR, 'index.js'),
+  output: {
+    path: DIST_DIR,
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
