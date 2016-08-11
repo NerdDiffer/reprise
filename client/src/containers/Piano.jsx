abee310f8ff7c0314642c2f5ecf6647251a3c095
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-11 15:48:06 -0700

    Quick patches in order to get the app running cleanly

---
+++ client/src/containers/Piano.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Piano = () => {
+  return (
+    <div>
+      I am a placeholder piano.
+      Am here in order to allow `UserInstrument` to render.
+    </div>
+  );
+};
+
+export default Piano;
