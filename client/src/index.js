29186d051a87924ae9c4613f1c172ad20dc299c5
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-11 16:08:14 -0700

    Add comment regarding material-ui
    Thought it'd be helpful in case we forget why it's there.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './containers/App.jsx';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 render(
