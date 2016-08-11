fc85eb85c266b6f37af0ca82bc435097684ac5d8
The TARDIS <loaner@Mr-Kittys-MacBook-Air.local>
2016-08-10 19:37:46 -0700

    Commit to rebase

---
+++ client/src/containers/FriendInstrument.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FriendInstrument extends Component {
+
+  render() {
+    return (
+      <div className='friendInstrument'>
+        Friends instrument goes here
+      </div>
+    );
+  }
+}
+
+export default FriendInstrument;
--- client/src/containers/Jamroom.jsx
+++ client/src/containers/Jamroom.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import UserInstrument from './UserInstrument';
+import FriendInstrument from './FriendInstrument'
 
 class JamRoom extends Component {
 
   render() {
     return (
       <div id="jamroom">
-      <h1>Welcome to the JamRoom!</h1>
-        <UserInstrument />
+        <h1>Welcome to the JamRoom!</h1>
+        <FriendInstrument />
+        <FriendInstrument /><br />								
+        <FriendInstrument />
+        <FriendInstrument />
+        <UserInstrument inst={this.props.inst} />
       </div>
     );
   }
