4081fa6cad3581d8de52aa36f3805ed55b706f1a
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-12 16:55:07 -0700

    Center things on LandingPage & turn into 'dumb' component

--- client/src/containers/LandingPage.jsx
+++ client/src/containers/LandingPage.jsx
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 
-const style = {
-  margin: 12,
+const materialStyles = {
+  margin: '0 auto',
+  outline: '1px dashed black',
+  display: 'block',
+  width: '25%'
 };
 
-class LandingPage extends Component {
-
-  render() {
-    return (
-      <div >
-        <p>WELCOME TO TBD!</p>
-         <br />
-        <RaisedButton label="Join Room" style={style} onClick={() => { this.props.change('selectInstrument')}} />
-        <br /><TextField /><br />
-        <RaisedButton label="Create Room" onClick={() => { this.props.change('selectInstrument')}} />
-      </div>
-    );
-  }
-}
+const LandingPage = (props) => (
+  <div id="landingPage">
+    <section className="title">
+      <h1>WELCOME TO TBD!</h1>
+    </section>
+    <section className="joinOrCreateRoom">
+      <RaisedButton
+        label="Join Room"
+        style={materialStyles}
+        onClick={() => { props.change('selectInstrument'); }}
+      />
+      <br />
+      <TextField style={materialStyles} />
+      <br />
+      <RaisedButton
+        label="Create Room"
+        style={materialStyles}
+        onClick={() => { props.change('selectInstrument'); }}
+      />
+    </section>
+  </div>
+);
 
 LandingPage.childContextTypes = {
   muiTheme: React.PropTypes.object.isRequired,
+};
+
+LandingPage.propTypes = {
+  change: React.PropTypes.function
 };
 
 export default LandingPage;
