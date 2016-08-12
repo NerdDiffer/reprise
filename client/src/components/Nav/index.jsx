140fd9e1636db34eea4079e0d6c0f0405b52d4af
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-12 11:30:54 -0700

    Split up IconMenu component from main Nav component

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 
 import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-
-const iconMenu = () => (
-  <IconMenu
-    iconButtonElement={
-      <IconButton><MoreVertIcon /></IconButton>
-    }
-    targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-    anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-  >
-    <MenuItem primaryText="Login" />
-    <MenuItem primaryText="Sign up" />
-    <MenuItem primaryText="Sign out" />
-  </IconMenu>
-);
+import NavIconMenu from './AppIconMenu';
 
 const AppNavBar = () => (
   <AppBar
     title="Jam App"
     showMenuIconButton={false}
-    iconElementRight={iconMenu()}
+    iconElementRight={<NavIconMenu />}
   />
 );
 
