0f791d7eadb2b4c947913d04276611dff35eaa95
Greg Roche <grroche.us@gmail.com>
2016-08-12 21:34:13 -0700

    Create invalid component page

---
+++ client/src/components/Invalid.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Invalid = () => (
+  <div className="invalid">
+    <img src="/assets/invalid_room.png" alt="" />
+    <h4>This room is does not exist</h4>
+  </div>
+);
+
+export default Invalid;
