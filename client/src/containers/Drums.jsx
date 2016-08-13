62f8df348726d9662081998a794467b30a40f65b
The TARDIS <loaner@Mr-Kittys-MacBook-Air.local>
2016-08-12 18:21:16 -0700

    Make drumset w visual rendering

---
+++ client/src/containers/Drums.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class Drums extends Component {
+  helper(ID) {
+    $(ID).animate({
+      backgroundColor: "silver",
+    }, 50).animate({
+      backgroundColor: "transparent"
+    }, 50);
+  }
+  render() {
+    return (
+      <div>
+        <img id = "cs" src="../../../style/DrumParts/completeSet.png" />
+	      <div onClick={() => { this.helper("#urC"); }} id="urC" />
+	      <div onClick={() => { this.helper("#nextC"); }} id="nextC" />
+	      <div onClick={() => { this.helper("#brDrum"); }} id="brDrum" />
+	      <div onClick={() => { this.helper("#rsDrum"); }} id="rsDrum" />
+	      <div onClick={() => { this.helper("#lsDrum"); }} id="lsDrum" />
+	      <div onClick={() => { this.helper("#tomDrum"); }} id="tomDrum" />
+	      <div onClick={() => { this.helper("#urCymbal"); }} id="urCymbal" />
+      </div>
+    );
+  }
+}
+
+export default Drums;
