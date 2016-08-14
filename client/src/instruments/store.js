c24c57f9277097453584622f193a31e55fafa820
Greg Roche <grroche.us@gmail.com>
2016-08-13 17:18:46 -0700

    Add piano instrument to piano store

--- client/src/instruments/store.js
+++ client/src/instruments/store.js
@@ -1,10 +1,12 @@
 // import piano
 // import fry
 import fry from 'fry';
+import piano from 'piano';
 // import piano from ''
 
 const store = {
-  fry: fry.playNote
+  fry: fry.playNote,
+  piano: piano.playNote
 };
 
 export default store;
