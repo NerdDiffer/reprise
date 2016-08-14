429e6b500838a6a97f845566fe1aadf33af6aec0
The TARDIS <loaner@Mr-Kittys-MacBook-Air.local>
2016-08-13 17:43:50 -0700

    Style landing page

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,22 +1,21 @@
+import AppBar from 'material-ui/AppBar';
 import React from 'react';
 import { Link } from 'react-router';
-import AppBar from 'material-ui/AppBar';
-import NavMenuIcon from './NavMenuIcon';
-import RaisedButton from 'material-ui/RaisedButton';
-const color={backgroundImage: 'url("http://bit.ly/2b2ePzs")', width: "100%",opacity: 0.6 };
+
+const color={ backgroundImage: 'url("http://bit.ly/2b2ePzs")', width: "100%", opacity: 0.6 };
 
 const AppNavBar = () => (
 
-  <div id ="navBar">
-  <AppBar
-   style={color}
-   showMenuIconButton={false}
-  >
-   <Link to="/"><img id="logo" src="http://bit.ly/2beSCQg" /></Link>
-  <Link to="login" ><button  className="navButtons"> Login!</button></Link>
-  <Link to="signup"><button  className="navButtons"> Signup!</button></Link>
- </AppBar >
-  
+  <div id="navBar">
+    <AppBar
+      style={color}
+      showMenuIconButton={false}
+    >
+      <Link to="/">
+        <img id="logo" src="http://bit.ly/2beSCQg" /></Link>
+      <Link to="login" ><button className="navButtons"> Login!</button></Link>
+      <Link to="signup"><button className="navButtons"> Signup!</button></Link>
+    </AppBar >
   </div>
 );
 
