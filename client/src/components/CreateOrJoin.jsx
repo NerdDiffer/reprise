a1fed5885540bd8a0d1742206c4c0e6c57b5d8e1
Greg Roche <grroche.us@gmail.com>
2016-08-16 16:00:10 -0700

    Build createorjoin component skeleton

---
+++ client/src/components/CreateOrJoin.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class CreateOrJoin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setState = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="I am a form" />
+          <input type="button" value="Click me broh" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateOrJoin;
