baebbb1d4f530958215bb92b2a204ffaf80d69cb
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-17 15:13:14 -0700

    Start on Dashboard for user

---
+++ client/src/components/Dashboard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+
+// Activate this dashboard by hitting an icon in upper-RH corner
+const Dashboard = (
+  <Drawer
+    openSecondary
+    docked={false}
+  />
+);
+
+export default Dashboard;
