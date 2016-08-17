713b0b7e273cc9960a56e38270f91b6ebb6fbc33
Greg Roche <grroche.us@gmail.com>
2016-08-16 19:06:23 -0700

    Implement form validation for entering new room names

--- client/src/components/CreateOrJoin.jsx
+++ client/src/components/CreateOrJoin.jsx
@@ -1,24 +1,94 @@
+// Modules
 import React, { Component } from 'react';
+import shortid from 'shortid';
+
+// Material UI
+import RaisedButton from 'material-ui/RaisedButton';
+
+// Local file imports
+import { socket } from '../peer';
 
 class CreateOrJoin extends Component {
   constructor(props) {
     super(props);
 
-    this.setState = {
-
+    this.state = {
+      createRoomVal: '',
+      showValidateError: false,
     };
+
+    this.handleCreateRoomClick = this.handleCreateRoomClick.bind(this);
+    this.handleCreateRoomChange = this.handleCreateRoomChange.bind(this);
+  }
+
+  handleCreateRoomClick(e) {
+    e.preventDefault();
+    let roomName;
+    if (this.state.createRoomVal.match(/[^a-zA-Z1-9]+/g)) {
+      this.setState({
+        showValidateError: true,
+      });
+      return;
+    }
+    if (this.state.createRoomVal === '') {
+      roomName = shortid.generate();
+    } else {
+      roomName = this.state.createRoomVal;
+      this.setState({
+        createRoomVal: '',
+      });
+    }
+
+    socket.emit('create room', roomName);
+    this.context.router.push(`room/${roomName}`);
+  }
+  // match nothing: ^(?![\s\S])
+  handleCreateRoomChange(e) {
+    this.setState({
+      createRoomVal: e.target.value
+    });
   }
 
   render() {
     return (
       <div>
-        <form>
-          <input type="text" placeholder="I am a form" />
-          <input type="button" value="Click me broh" />
-        </form>
+        <div id="createView" style={{ position: 'absolute', top: '20%', left: '50%', transform: 'translate(-65%, -50%)', textAlign: 'center' }}>
+          <div>
+            If you can't think of a good room name, just click "Create Room brog"
+             and we will provide you with a random room name.
+          </div>
+          <form>
+            <input
+              type="text"
+              value={this.state.createRoomVal}
+              onChange={this.handleCreateRoomChange}
+              placeholder="Enter a Room Name..."
+              pattern="[a-zA-Z1-9]+"
+              style={{ height: '32px' }}
+            />
+            <RaisedButton onClick={this.handleCreateRoomClick} label="Create Room broh" />
+          </form>
+          {
+            this.state.showValidateError
+            ? <div>
+              Room names can only contain letters or numbers.
+               Please enter correct combination of character to make room
+            </div>
+            : null
+          }
+        </div>
+        <div id="join-view" style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-65%, -50%)', textAlign: 'center' }} >
+          <div>
+            Have a link already?  Just paste it into your url bar!  Otherwise, checkout the open rooms below.  Click to join!
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+CreateOrJoin.contextTypes = {
+  router: React.PropTypes.object
+};
 
 export default CreateOrJoin;
