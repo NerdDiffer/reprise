95179114f1e880e043686ed53ab0292726e64ec4
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-18 20:25:08 -0700

    Add toggling, presentational functionality to BeatSequencer

--- client/src/components/BeatSequencer/Row.jsx
+++ client/src/components/BeatSequencer/Row.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import classnames from 'classnames';
 
 /**
  * - receives props from parent `Sequence`
  * - renders each subdivision of the beat
  */
-const Row = ({ sound, sequence }) => {
+const Row = ({ sound, sequence, handleClick }) => {
+
+  const renderBeat = (beat, index) => {
+    const beatStyle = classnames({ selected: beat === 1 }, 'beatBox');
+    const _handleClick = handleClick.bind(null, index);
+
+    return (
+      <div className={beatStyle} id={index} onClick={_handleClick}></div>
+    )
+  };
+
   return (
     <div className="beatSequence row">
-      <div className="beatBox"></div>
-      <div className="beatBox"></div>
-      <div className="beatBox"></div>
-      <div className="beatBox"></div>
+      { sequence.map((beat, index) => renderBeat(beat, index)) }
     </div>
   );
 };
--- client/src/components/BeatSequencer/Sequence.jsx
+++ client/src/components/BeatSequencer/Sequence.jsx
@@ -12,12 +12,24 @@ class Sequence extends Component {
     super(props);
 
     this.state = {
-
+      sequence: [0, 0, 0, 0]
     };
+
+    this.toggleBeat = this.toggleBeat.bind(this);
   }
+
+  toggleBeat(index) {
+    const sequence = this.state.sequence;
+    const newValue = sequence[index] === 0 ? 1 : 0;
+
+    this.setState({
+      sequence: [...sequence.slice(0, index), newValue, ...sequence.slice(index + 1)]
+    });
+  }
+
   render() {
     return (
-      <Row />
+      <Row sequence={this.state.sequence} handleClick={this.toggleBeat} />
     );
   }
 };
