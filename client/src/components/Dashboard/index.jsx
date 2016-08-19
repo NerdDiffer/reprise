967560c9b09736f0662c638f49e2698581bc93fc
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-19 15:29:17 -0700

    Update Dashboard component

--- client/src/components/Dashboard/index.jsx
+++ client/src/components/Dashboard/index.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import Drawer from 'material-ui/Drawer';
 
 // Activate this dashboard by hitting an icon in upper-RH corner
-const Dashboard = (
+const Dashboard = ({ open }) => (
   <Drawer
     openSecondary
     docked={false}
-  />
+    open={open}
+    onRequestChange={(open) => this.setState({open})}
+  >
+    <p>Item 1</p>
+    <p>Item 2</p>
+    <p>Item 3</p>
+  </Drawer>
 );
 
 export default Dashboard;
