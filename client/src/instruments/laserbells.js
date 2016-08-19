f9cfa82930374f8e795b35c22834bd4d34ad7f93
Frances <franyang99@gmail.com>
2016-08-19 14:41:51 -0700

    Edit laserbells so it's only one sound

--- client/src/instruments/laserbells.js
+++ client/src/instruments/laserbells.js
@@ -1,23 +1,9 @@
-import basso from './sounds/basso';
-import wimbal from './sounds/wimbal';
 import zwill from './sounds/zwill';
-
-const lowKeys = new Set('asdfgh');
-
-// zimits
-const midKeys = new Set('zxcvbnmjkl');
 
 const playNote = keyPressed => {
   // coerce to string
   keyPressed += '';
-
-  if (lowKeys.has(keyPressed)) {
-    basso(keyPressed);
-  } else if (midKeys.has(keyPressed)) {
-    zwill(keyPressed);
-  } else {
-    wimbal(keyPressed);
-  }
+  zwill(keyPressed);
 };
 
 export default { playNote };
