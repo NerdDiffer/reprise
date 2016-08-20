7aa8dc1e6981e94c4a04cf74ceb8c7596f0a4ff7
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-20 12:31:10 -0700

    Fix Link components for NavMenuIcon

--- client/src/components/Nav/NavMenuIcon.jsx
+++ client/src/components/Nav/NavMenuIcon.jsx
@@ -5,7 +5,8 @@ import IconButton from 'material-ui/IconButton';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
-// Extra options, as dots on RH-side
+// How to wrap `MenuItem` components inside of `Link` components without warnings?
+// https://github.com/callemall/material-ui/issues/4899
 const NavMenuIcon = () => (
   <IconMenu
     iconButtonElement={
@@ -16,12 +17,14 @@ const NavMenuIcon = () => (
     targetOrigin={{ horizontal: 'right', vertical: 'top' }}
     anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
   >
-    <MenuItem primaryText="Login">
-      <Link to="login" />
-    </MenuItem>
-    <MenuItem primaryText="Sign up">
-      <Link to="signup" />
-    </MenuItem>
+    <MenuItem
+      primaryText="Login"
+      containerElement={<Link to="/login" />}
+    />
+    <MenuItem
+      primaryText="Sign up"
+      containerElement={<Link to="/signup" />}
+    />
   </IconMenu>
 );
 
