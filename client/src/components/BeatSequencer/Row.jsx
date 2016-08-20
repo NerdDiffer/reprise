8c8fcba18f112716a6915d4b02146e9b37d3203e
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-20 15:46:57 -0700

    Update BeatSequencer/Row component

--- client/src/components/BeatSequencer/Row.jsx
+++ client/src/components/BeatSequencer/Row.jsx
@@ -5,20 +5,20 @@ import classnames from 'classnames';
  * - receives props from parent `Sequence`
  * - renders each subdivision of the beat
  */
-const Row = ({ sound, sequence, handleClick }) => {
+const Row = ({ sound, events, handleClick }) => {
 
   const renderBeat = (beat, index) => {
     const beatStyle = classnames({ selected: beat === 1 }, 'beatBox');
     const _handleClick = handleClick.bind(null, index);
 
     return (
-      <div className={beatStyle} id={index} onClick={_handleClick}></div>
+      <div className={beatStyle} key={index} onClick={_handleClick}></div>
     )
   };
 
   return (
     <div className="beatSequence row">
-      { sequence.map((beat, index) => renderBeat(beat, index)) }
+      { events.map((beat, index) => renderBeat(beat, index)) }
     </div>
   );
 };
