e2795b14ec41d1377d123a52a1a474af8c39580e
Frances <franyang99@gmail.com>
2016-08-20 17:59:21 -0700

    Render correct number of bubbles

--- client/src/components/PeerBar.jsx
+++ client/src/components/PeerBar.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 
 import PeerBubble from './PeerBubble';
 
-const PeerBar = ({ peers, toggleInviteView }) => (
-  <div className="peer-bar">
-    {
-      [true, false, true, false].map((peer, index) => (
-        <div key={index}>
-          <PeerBubble peer={peer} handleClick={toggleInviteView} />
-        </div>
-      ))
-    }
-  </div>
-);
+const PeerBar = ({ ownInstrument, peers, toggleInviteView }) => {
+  const inviteNumber = 3 - peers.length;
+  const inviteArray = [];
+  for (let i = 0; i < inviteNumber; i++) {
+    inviteArray.push(i);
+  }
+  return (
+    <div className="peer-bar">
+      <PeerBubble peer={{ instrument: ownInstrument, name: 'Me' }} />
+      {
+        peers.map(peer => (
+          <div key={peer.peerId}>
+            <PeerBubble peer={peer} />
+          </div>
+        ))
+      }
+      {
+        inviteArray.map(index => (
+          <div key={index}>
+            <PeerBubble peer={{}} handleClick={toggleInviteView} />
+          </div>
+        ))
+      }
+    </div>
+  );
+};
 
 PeerBar.propTypes = {
   peers: React.PropTypes.array.isRequired,
-  toggleInviteView: React.PropTypes.func.isRequired
+  toggleInviteView: React.PropTypes.func.isRequired,
+  ownInstrument: React.PropTypes.string.isRequired
 };
 
 export default PeerBar;
