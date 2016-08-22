45e188917a120425b38f72033f79a34df17522d4
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-22 15:30:23 -0700

    Add comments to a tone js sequence object

--- client/src/instruments/beats/sequence.js
+++ client/src/instruments/beats/sequence.js
@@ -5,6 +5,10 @@ import { Sequence } from 'tone';
  * For details, see: https://tonejs.github.io/docs/#Sequence
  *
  * @param { tone }, A note to play on every event
+ *   If the sound definition (`def`) is a MembraneSynth, then pass a note
+ *   string, such as: `Bb4`. Again, consult the ToneJS docs for details.
+ *   If the sound defintion is a MetalSynth, pass a frequency, such as: `200`.
+ *   Consult ToneJS docs for more info.
  * @param { def }, Sound definition. Could be something like a
  *   Tone.MembraneSynth, Tone.MetalSynth object
  * @param events, {Array} sequence of events to play
