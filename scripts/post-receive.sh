41ed8e945255623d51292a3bb3aa9abfd4c2f757
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-24 21:41:07 -0700

    Add a post-receive script for .git dir

---
+++ scripts/post-receive.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Put this script in your git hooks repo
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-automatic-deployment-with-git-with-a-vps
+
+WORK_TREE=$HOME/app/
+GIT_DIR=$HOME/app.git
+mkdir $WORK_TREE
+mkdir $GIT_DIR
+
+git --work-tree=$WORK_TREE --git-dir=$GIT_DIR checkout -f
+
+cd $WORK_TREE
+npm start
