4d43f4005e5e2c233a8330d7b1519377ad38e41f
The TARDIS <loaner@Mr-Kittys-MacBook-Air.local>
2016-08-25 19:59:18 -0700

    Work on Drums

--- client/src/components/Drums.jsx
+++ client/src/components/Drums.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { mapDrumIDToSounds } from '../utils/helperFunctions'
-import Tone from 'Tone'
-
+import { Buffer } from "tone";
+import { mapDrumIDToSounds } from '../utils/helperFunctions';
 
 class Drums extends Component {
   helper(ID) {
 // Run mapDrumIDToSounds
+
+document.getElementById('audiotag1').play();
+
 
     $(ID).animate({
       backgroundColor: "silver",
@@ -18,13 +20,20 @@ class Drums extends Component {
     return (
       <div id="userDrums">
         <img id="cs" src="../../../style/DrumParts/completeSet.png" />
-	      <div onClick={() => { this.helper("#urC"); }} id="urC" />
-	      <div onClick={() => { this.helper("#nextC"); }} id="nextC" />
-	      <div onClick={() => { this.helper("#brDrum"); }} id="brDrum" />
-	      <div onClick={() => { this.helper("#rsDrum"); }} id="rsDrum" />
-	      <div onClick={() => { this.helper("#lsDrum"); }} id="lsDrum" />
-	      <div onClick={() => { this.helper("#tomDrum"); }} id="tomDrum" />
-	      <div onClick={() => { this.helper("#urCymbal"); }} id="urCymbal" />
+        <div onClick={() => { this.helper("#urC"); }} id="urC" />
+        <div onClick={() => { this.helper("#nextC"); }} id="nextC" />
+        <div onClick={() => { this.helper("#brDrum"); }} id="brDrum" />
+        <div onClick={() => { this.helper("#rsDrum"); }} id="rsDrum" />
+        <div onClick={() => { this.helper("#lsDrum"); }} id="lsDrum" />
+        <div onClick={() => { this.helper("#tomDrum"); }} id="tomDrum" />
+        <div onClick={() => { this.helper("#urCymbal"); }} id="urCymbal" />
+      <audio id="audiotag1">
+        <source src="http://www.freesfx.co.uk/rx2/mp3s/3/15442_1460468286.mp3"/>
+      </audio>
+      <audio id="audiotag2">
+        <source src="http://www.freesfx.co.uk/rx2/mp3s/2/14309_1459962858.mp3"/>
+      </audio>
+      
       </div>
     );
   }
