012ad1d32e4bf6d77ee78592d814a2d4fa7abf42
The TARDIS <loaner@Mr-Kittys-MacBook-Air.local>
2016-08-26 09:46:21 -0700

    Comment out drum sound logic

--- client/src/components/Drums.jsx
+++ client/src/components/Drums.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { Buffer } from "tone";
-import { mapDrumIDToSounds } from '../utils/helperFunctions';
+// import { mapDrumIDToSounds } from '../utils/helperFunctions';
 
 class Drums extends Component {
   helper(ID) {
 // Run mapDrumIDToSounds
-
-document.getElementById('audiotag1').play();
+// document.getElementById('audiotag1').play();
 
 
     $(ID).animate({
@@ -27,16 +26,17 @@ document.getElementById('audiotag1').play();
         <div onClick={() => { this.helper("#lsDrum"); }} id="lsDrum" />
         <div onClick={() => { this.helper("#tomDrum"); }} id="tomDrum" />
         <div onClick={() => { this.helper("#urCymbal"); }} id="urCymbal" />
-      <audio id="audiotag1">
-        <source src="http://www.freesfx.co.uk/rx2/mp3s/3/15442_1460468286.mp3"/>
-      </audio>
-      <audio id="audiotag2">
-        <source src="http://www.freesfx.co.uk/rx2/mp3s/2/14309_1459962858.mp3"/>
-      </audio>
-      
       </div>
     );
   }
 }
 
 export default Drums;
+
+
+// <audio id="audiotag1">
+//         <source src="http://www.freesfx.co.uk/rx2/mp3s/3/15442_1460468286.mp3"/>
+//       </audio>
+//       <audio id="audiotag2">
+//         <source src="http://www.freesfx.co.uk/rx2/mp3s/2/14309_1459962858.mp3"/>
+//       </audio>
