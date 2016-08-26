52545401b79f74df292f20eab223bb364e58fee6
Frances <franyang99@gmail.com>
2016-08-26 11:35:42 -0700

    Add drums to store

--- client/src/instruments/store.js
+++ client/src/instruments/store.js
@@ -1,9 +1,11 @@
 import laserbells from './laserbells';
 import piano from './piano';
+import drums from './drums';
 
 const store = {
   laserbells: laserbells.playNote,
-  piano: piano.playNote
+  piano: piano.playNote,
+  drums: drums.playNote
 };
 
 const instruments = ['piano', 'drums', 'laserbells'];
