87108eb187cdde0517f5134be3a474b69ff8166a
Rafael Espinoza <nerddiffer@gmail.com>
2016-10-09 15:38:57 -0700

    Refactor handlers for '/api/rooms'
    fix/update(server rooms) Fix server-side bugs & update message
    
    - Reference correct column names.
    - Send better response code & fix params for creating rooms
    - Update message for creating a new room
      - Reference new room url instead of room name

--- server/api/rooms.js
+++ server/api/rooms.js
@@ -1,49 +1,54 @@
-const { users, PrivateRooms } = require('../db/models');
+const { User, PrivateRoom } = require('../db/models');
 const { isLoggedIn, readSession } = require('../auth/sessionHelpers');
 
 // POST `/api/rooms/`
 module.exports.createPrivateRoom = (req, res) => {
   if (!isLoggedIn(req)) {
-    res.send('you must be logged in');
+    res.status(400).json('You must be logged in');
   } else {
-    users.findOne({
-      where: {
-        userName: readSession(req)
-      }
+    const { name } = req.body;
+
+    User.findOne({
+      where: { name: readSession(req) }
     })
-    .then((user) => {
-      // if query came back empty handed then user must be logged in via facebook, and their id in schema is stored in passport
-      const userId = user ? user.id : req.session.passport.user;
-      return PrivateRooms.create({
-        url: req.body.roomName,
-        userId,
+    .then(user => {
+      // if query came back empty handed then user must be logged in via
+      // facebook,  and their id in schema is stored in passport
+      const user_id = user ? user.id : req.session.passport.user;
+
+      return PrivateRoom.create({
+        url: name,
+        user_id
       });
     })
-    .then(() => {
-      res.sendStatus(200);
+    .then(newRoom => {
+      const msg = `Successfully created room: ${newRoom.url}`;
+      res.status(201).json(msg);
     });
   }
 };
 
 // GET `/api/rooms/`
 module.exports.listPrivateRooms = (req, res) => {
+  // TODO: see if clients w/o accounts are using this endpoint
+  // If not, then check if client is logged in first before moving on.
   // is it not a facebook user?
-  users.findOne({
+  User.findOne({
     where: {
-      userName: readSession(req)
+      name: readSession(req)
     }
   })
   .then(user => {
-    // if query came back empty handed then user must be logged in via facebook, and their id in schema is stored in passport
-    const userId = user ? user.id : req.session.passport.user;
-    return PrivateRooms.findAll({
-      where: {
-        userId,
-      }
+    // if query came back empty handed then user must be logged in via
+    // facebook, and their id in schema is stored in passport
+    const user_id = user ? user.id : req.session.passport.user;
+
+    return PrivateRoom.findAll({
+      where: { user_id }
     });
   })
   .then(privateRooms => {
-    // get url
-    res.send(privateRooms.map(room => room.url));
+    const urls = privateRooms.map(room => room.url);
+    res.status(200).json(urls);
   });
 };
