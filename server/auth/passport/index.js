6e24d4c4aead67d3b499bc38a81b134ed6c2ac8c
Rafael Espinoza <nerddiffer@gmail.com>
2016-10-17 12:00:21 -0700

    feat(tokens): Connect strategies to passport
    When using multiple strategies, need to use a name for each one.

--- server/auth/passport/index.js
+++ server/auth/passport/index.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 const facebookStrategy = require('./facebookStrategy');
+const jwtStrategy = require('./jwtStrategy');
+const localStrategy = require('./localStrategy');
 
 passport.use('facebook', facebookStrategy);
+passport.use('jwt', jwtStrategy);
+passport.use('local', localStrategy);
 
 module.exports = passport;
