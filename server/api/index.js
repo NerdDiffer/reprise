f5295e79ef25437068b165732e0e2f2035ff9f25
Rafael Espinoza <nerddiffer@gmail.com>
2016-10-17 13:36:42 -0700

    feat(passport): Use passport middleware in API
    Add two middleware helper functions.
    You can put them before any routes & handlers you need to protect.

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,28 @@
 /* eslint new-cap: 'off' */
 const { Router } = require('express');
+const passport = require('../auth/passport');
 const accounts = require('./accounts');
 const oauth = require('./oauth');
 const rooms = require('./rooms');
 
+/* Authorization middleware */
+
+// Require the user to have a valid JWT before accessing any protected resources
+const requireAuth = passport.authenticate('jwt', {
+  session: false
+});
+
+// Validate user's name & password. Do this before sending user a token.
+const validateLogin = passport.authenticate('local', {
+  session: false
+});
+
+/* Routes */
+
 const router = Router();
 
 // Login, logout
-router.post('/api/login', accounts.login);
+router.post('/api/login', validateLogin, accounts.login);
 router.get('/api/logout', accounts.logout);
 
 // Accounts: `/api/accounts/`
