81f2ea7df4add7a194d3c72b0cbd43c104fa31d8
Rafael Espinoza <nerddiffer@gmail.com>
2016-10-19 18:14:32 -0700

    feat(tokenExchange): Generate long lived tokens
    - Meant to exchange a short-lived token for long-lived token.
      - May or may not keep this thing, depending on functionality provided
        by the passport-facebook-token module.
    
    Fixes:
    
    - fixed incorrect handling response in Axios
    - Add some debugging tools, statements
    - send proper HTTP status code when there's an error

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ const passport = require('../auth/passport');
 const accounts = require('./accounts');
 const oauth = require('./oauth');
 const rooms = require('./rooms');
+const tokens = require('./tokenExchange');
 
 /* Authorization middleware */
 
@@ -28,16 +29,20 @@ router.get('/api/logout', accounts.logout);
 // Accounts: `/api/accounts/`
 router.post('/api/accounts', accounts.signup);
 
-// OAuth: `/api/oauth/`
+// OAuth & tokens: `/api/oauth/`
 router.get('/api/oauth/facebook', oauth.facebook);
 router.get('/api/oauth/facebook/callback', oauth.facebookCallback);
-router.post('/api/oauth/facebook/token',
+router.post('/api/oauth/passport-facebook-token',
   oauth.facebookToken,
   (req, res) => {
     const msg = req.user ? 200 : 401;
     res.send(msg);
   }
 );
+router.post('/api/oauth/facebook/token', tokens.generateLongLivedToken);
+
+// alt token generation
+router.post('/api/tokens', tokens.generateLongLivedToken);
 
 // Private rooms: `/api/rooms/`
 router.post('/api/rooms', requireAuth, rooms.createPrivateRoom);
---
+++ server/api/tokens.js
@@ -0,0 +1,67 @@
+const debug = require('debug')('reprise');
+/**
+ * https://developers.facebook.com/docs/facebook-login/access-tokens/expiration-and-extension
+ */
+const Axios = require('axios');
+
+require('dotenv').config();
+const { clientID, clientSecret } = process.env;
+
+// configure axios client
+const VERSION = '2.8';
+const axios = Axios.create({
+  baseURL: `https://graph.facebook.com/v${VERSION}`
+});
+
+// debugging helper
+const debugAxiosErr = err => {
+  debug('err.message:\n %s', err.message);
+  debug('err.config:\n %o', err.config);
+
+  if (err.response) {
+    const { status, statusText, headers, config } = err.response;
+
+    debug('err.response.status:\n %o', status);
+    debug('err.response.statusText:\n %o', statusText);
+    debug('err.response.headers:\n %o', headers);
+    debug('err.response.config:\n %o', config);
+  }
+};
+
+module.exports.generateLongLivedToken = (req, res, next) => {
+  const shortLivedToken = req.body.accessToken;
+
+  if (!shortLivedToken) {
+    return res.status(400).json('Send a short-lived at: accessToken');
+  }
+
+  debug('shortLivedToken: %s', shortLivedToken);
+
+  const params = {
+    grant_type: 'fb_exchange_token',
+    client_id: clientID,
+    client_secret: clientSecret,
+    fb_exchange_token: shortLivedToken
+  };
+
+  const url = `/oauth/access_token`;
+
+  //GET /oauth/access_token?
+  //  grant_type=fb_exchange_token&amp;
+  //  client_id={app-id}&amp;
+  //  client_secret={app-secret}&amp;
+  //  fb_exchange_token={short-lived-token}
+  return axios.get(url, { params })
+    .then(fbResponse => {
+      const msg = fbResponse.data;
+      return res.json(msg);
+    })
+    .catch(err => {
+      debugAxiosErr(err);
+
+      const statusCode = err.response ? err.response.status : 400;
+      const msg = err.response ? err.response.data : err.message;
+
+      return res.status(statusCode).json(msg);
+    });
+};
